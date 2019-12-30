e632be20e705fb2e32fce7125d90bebc7627cd86
Bikram Tuladhar <bikramtuladhar2011@gmail.com>
2019-12-30 11:11:29 +0545

    init

---
+++ run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+docker network create mysql
+
+if [ "$1" == "" ]; then
+    for d in ./*; do
+	  if [ -d "$d" ]; then
+	    docker-compose -f "$d"/docker-compose.yml up -d
+	  fi
+	done
+fi
+
+if [ "$1" != "" ]; then
+    for d in ./*; do
+	  if [ -d "$d" ]; then
+	    docker-compose -f "$d"/docker-compose.yml "$1"
+	  fi
+	done
+fi
